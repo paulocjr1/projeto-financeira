0f32ce4e6c25960e0e8daeda798f5311f0c43fb5
paulo <paulocesarjunior605@gmail.com>
2025-03-25 04:32:19 -0300

    Atualização projeto

--- clientes/views.py
+++ clientes/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect
 from .forms import ClienteForm
+from .models import Cliente
 # Create your views here.
 
 def cadastrar_cliente(request):
+    form = ClienteForm()
     if request.method == 'POST':
         form = ClienteForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('lista_clientes')
-    else:
-        form = ClienteForm()
+            return redirect('cadastrar_cliente')
     
-    return render(request, 'cadastrar_cliente.html', {'form': form})
+    clientes = Cliente.objects.all()
+    return render(request, 'cadastrar_cliente.html', {'form': form, 'clientes': clientes})
